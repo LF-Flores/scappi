219993f93c8ee01bb26e4a4bd1c807edefe475d2
Felipe flores <lffm.fismat@gmail.com>
2022-11-20 09:47:11 -0600

    cleaning

--- scappi_core/build/utils.d.ts
+++
@@ -1,3 +0,0 @@
-declare function random_id(size?: number): string;
-export { random_id };
-//# sourceMappingURL=utils.d.ts.map
--- scappi_core/src/index.ts
+++ scappi_core/src/index.ts
@@ -8,9 +8,15 @@ const user = new Requester()
 // Creating request with the available validators.
 const request = user.create_signup_request(available_validators)
 
+// Requests
+const signup = user.create_signup_request(available_validators)
+const auth = user.create_authentication_request("0x850fC23aCC279352e7d7793a011742eC8743b2a7")
+
 // Exploring 
 const selected = request.selected_validators
 
 console.log(request, "\n")
 console.log(available_validators, "\n")
+console.log(signup, "\n")
+console.log(auth, "\n")
 console.log("Selected validators: ", selected, "\n")
